98caae0a5ceaf647cd68e23247f4e321b418e3a7
Yashodipundre <yashundre07@gmail.com>
2024-04-03 18:41:46 +0530

    New

--- wtPro/Routers/service.js
+++ wtPro/Routers/service.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const ServiceController = require('../Controller/Service')
+const ServiceController = require('../Controller/venue')
 const Router = express.Router()
 
 Router
-    .get('/venues', ServiceController.getVenues)
+    .get('/venues/All', ServiceController.getVenues)
+    .get('/venues/lawns',ServiceController.getLawnsvanue)
+    .get('/venues/FarmHouse',ServiceController.getFarmHouseVenues)
 
 exports.router = Router;
