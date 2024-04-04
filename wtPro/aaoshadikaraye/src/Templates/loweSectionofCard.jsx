26bdaa978b588de1f5cbb144450fafb3be6be0c1
Uzair-Tajmat <uzairtajmt7@gmail.com>
2024-04-04 13:25:29 +0530

    Updated VenuePage

--- wtPro/aaoshadikaraye/src/Templates/loweSectionofCard.jsx
+++ wtPro/aaoshadikaraye/src/Templates/loweSectionofCard.jsx
@@ -13,7 +13,7 @@ const LowerSection = ({ venue, rating, location, type, veg, nonVeg,img }) => {
         <h4 className={styles.customEditFLine}>
           <strong>{venue}</strong>
         </h4>
-        <div className={styles.rating}>
+        <div className={styles.ratingOG}>
           <h5>
             <FaRegStar className={styles.icon}  />
             {rating}
@@ -22,31 +22,18 @@ const LowerSection = ({ venue, rating, location, type, veg, nonVeg,img }) => {
       </div>
       <div className={styles.secondLine}>
         <h4 className={styles.customEditSLine}>
-          <CiLocationOn />
+          <CiLocationOn className={styles.icon} />
           {location}
         </h4>
+     
+      </div>
+
+      <div className={styles.thirdLine}>
         <div className={styles.rating}>
           <h5 className={styles.typeof}>
             <FaBuildingColumns className={styles.icon} />
             {type}
           </h5>
-        </div>
-      </div>
-
-      <div className={styles.thirdLine}>
-        <div className={styles.block1}>
-          <h5 className={styles.customEditTLine}>Veg</h5>
-          <h3 className={styles.price}>
-            <FaIndianRupeeSign className={styles.iconWidth} />
-            {veg}
-          </h3>
-        </div>
-        <div className={styles.block1}>
-          <h5 className={styles.customEditTLine}>Non-Veg</h5>
-          <h3 className={styles.price}>
-            <FaIndianRupeeSign className={styles.iconWidth} />
-            {nonVeg}
-          </h3>
         </div>
       </div>
     </div>
